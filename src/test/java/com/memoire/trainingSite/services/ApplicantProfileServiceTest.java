a07c0ca08d18070da8d22dfc78470083ea353c21
HamidaBz <hami.bouaziz@gmail.com>
2024-09-09 18:16:07 +0200

    test applicant profile service

--- src/test/java/com/memoire/trainingSite/services/ApplicantProfileServiceTest.java
+++ src/test/java/com/memoire/trainingSite/services/ApplicantProfileServiceTest.java
@@ -2,11 +2,10 @@ package com.memoire.trainingSite.services;
 
 import com.memoire.trainingSite.DAO.ApplicantProfileRepo;
 import com.memoire.trainingSite.Services.ApplicantProfileService;
-import com.memoire.trainingSite.models.Applicant;
 import com.memoire.trainingSite.models.ApplicantProfile;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -14,9 +13,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class ApplicantProfileServiceTest {
@@ -85,6 +82,49 @@ class ApplicantProfileServiceTest {
         assertThat(result).isEmpty();
     }
     @Test
-    void updateProfile() {
+    void test_updateProfile_if_profile_exists() {
+        //given
+        Long profile_id = 1L;
+
+        ApplicantProfile existingApplicantProfile = new ApplicantProfile();
+        existingApplicantProfile.setProfile_id(profile_id);
+        existingApplicantProfile.setProfile_intro("former Intro");
+        ApplicantProfile updatedApplicantProfile = new ApplicantProfile();
+        updatedApplicantProfile.setProfile_id(profile_id);
+        updatedApplicantProfile.setProfile_intro("new Intro");
+
+        when(applicantProfileRepo.findById(profile_id)).thenReturn(Optional.of(existingApplicantProfile));
+        when(applicantProfileRepo.save(updatedApplicantProfile)).thenReturn(updatedApplicantProfile);
+
+        //when
+        Optional<ApplicantProfile> result =
+                applicantProfileService.updateProfile(profile_id, updatedApplicantProfile);
+        //then
+        assertThat(result).isPresent();
+        verify(applicantProfileRepo).findById(profile_id);
+        ArgumentCaptor<ApplicantProfile> captorProfile = ArgumentCaptor.forClass(ApplicantProfile.class);
+        verify(applicantProfileRepo).save(captorProfile.capture());
+        assertThat(captorProfile.getValue()).isEqualTo(updatedApplicantProfile);
+        assertThat(result.get()).isEqualTo(updatedApplicantProfile);
+    }
+
+    @Test
+    void test_updateProfile_if_profile_does_not_exist() {
+        //given
+        Long profile_id = 1L;
+
+        ApplicantProfile updatedApplicantProfile = new ApplicantProfile();
+        updatedApplicantProfile.setProfile_id(profile_id);
+        updatedApplicantProfile.setProfile_intro("new Intro");
+
+        when(applicantProfileRepo.findById(profile_id)).thenReturn(Optional.empty());
+        //when
+        Optional<ApplicantProfile> result =
+                applicantProfileService.updateProfile(profile_id, updatedApplicantProfile);
+        //then
+        verify(applicantProfileRepo).findById(profile_id);
+        verify(applicantProfileRepo, never()).save(updatedApplicantProfile);
+        assertThat(result).isEmpty();
+
     }
 }
